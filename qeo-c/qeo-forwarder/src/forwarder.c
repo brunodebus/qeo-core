a364a3a3632dedc0ff875f1afc3de196336ce476
William Woodall <william@osrfoundation.org>
2014-09-18 15:32:41 -0700

    qeo-forwarder: use clock_get_time on os x

--- qeo-c/qeo-forwarder/src/forwarder.c
+++ qeo-c/qeo-forwarder/src/forwarder.c
@@ -34,6 +34,11 @@
 #include <qeo/log.h>
 #include "forwarder.h"
 
+#ifdef __MACH__
+#include <mach/clock.h>
+#include <mach/mach.h>
+#endif
+
 /* delay in milliseconds */
 #define UPNP_DELAY 2000
 
@@ -455,7 +460,17 @@ static void update_timer(struct timespec *timer,
                          timer_type_t type,
                          int sec_period)
 {
-    clock_gettime(CLOCK_REALTIME, &timer[type]);
+# ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
+  clock_serv_t cclock;
+  mach_timespec_t mts;
+  host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+  clock_get_time(cclock, &mts);
+  mach_port_deallocate(mach_task_self(), cclock);
+  timer[type].tv_sec = mts.tv_sec;
+  timer[type].tv_nsec = mts.tv_nsec;
+# else
+  clock_gettime(CLOCK_REALTIME, &timer[type]);
+# endif
     timer[type].tv_sec += sec_period;
 }
 
